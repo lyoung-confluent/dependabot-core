6380486ba0e2cdaae36d134aed3f9decc2f14226
Alfredo Cerezo <alcere@github.com>
2022-12-02 16:02:44 +0100

    Clean up linters

--- python/spec/dependabot/python/file_parser/pyproject_files_parser_spec.rb
+++ python/spec/dependabot/python/file_parser/pyproject_files_parser_spec.rb
@@ -305,7 +305,7 @@ RSpec.describe Dependabot::Python::FileParser::PyprojectFilesParser do
 
       subject(:dependencies) { parser.dependency_set.dependencies }
 
-      its(:length) { is_expected.to be > 0 }
+      its(:length) { is_expected.to be.positive? }
     end
   end
 end
